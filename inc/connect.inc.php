39daa41901175dd48e898876f22c15b6b86c247c
PeterCarloooo <43430291+PeterCarloooo@users.noreply.github.com>
2018-11-25 10:54:36 +0800

    Update connect.inc.php

--- inc/connect.inc.php
+++ inc/connect.inc.php
@@ -1,6 +1,6 @@
 <?php 
 mysql_connect("ftp.gear.host","gtt-lgbtcommunity-github\$gtt-lgbtcommunity-github","R1hCMa4wsXSnCzqWw3HHHBJrp53cqMzZ83QNSoM4huuePzlyQnyzprt5Wa99") or die("Couldn't connet to SQL server");
-mysql_select_db("lgbt_db") or die("Couldn't select DB");
+mysql_select_db("lgbtdb") or die("Couldn't select DB");
 
 //time formate
 function formatDate($date){

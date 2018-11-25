a7c75abd6ba5d368e243cd90534143e2195ef9d3
PeterCarloooo <43430291+PeterCarloooo@users.noreply.github.com>
2018-11-25 10:55:43 +0800

    Updassda

--- inc/connect.inc.php
+++ inc/connect.inc.php
@@ -1,5 +1,5 @@
 <?php 
-mysql_connect("ftp.gear.host","gtt-lgbtcommunity-github\$gtt-lgbtcommunity-github","R1hCMa4wsXSnCzqWw3HHHBJrp53cqMzZ83QNSoM4huuePzlyQnyzprt5Wa99") or die("Couldn't connet to SQL server");
+mysql_connect("ftp.gear.host","gtt-lgbtcommunity-github/$gtt-lgbtcommunity-github","R1hCMa4wsXSnCzqWw3HHHBJrp53cqMzZ83QNSoM4huuePzlyQnyzprt5Wa99") or die("Couldn't connet to SQL server");
 mysql_select_db("lgbtdb") or die("Couldn't select DB");
 
 //time formate
